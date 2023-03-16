e461b7b86b16f86074e4e837bc0ea904018153f7
Shahid Arshad <shahid.arshad@devsinc.com>
2023-03-16 09:01:53 -0400

    Initial Prject Setup

---
+++ RPS Game/RPS Game/RPS_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPS_GameApp.swift
+//  RPS Game
+//
+//  Created by Dev on 16/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct RPS_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
